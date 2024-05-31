ef96d9791bdf2f5b556ffa0a42da426bfd363e5f
jandam15 <97833075+jandam15@users.noreply.github.com>
2024-05-31 18:57:13 +0200

    Add message to empty result when searching products by description

--- src/main/kotlin/dk/cngroup/wishlist/controller/ProductController.kt
+++ src/main/kotlin/dk/cngroup/wishlist/controller/ProductController.kt
@@ -5,11 +5,7 @@ import dk.cngroup.wishlist.entity.ProductRepository
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 //classic Spring MVC controller
 @RestController
@@ -19,10 +15,10 @@ class ProductController(private val repository: ProductRepository) {
     @GetMapping("/products/search/findByDescription")
     fun getProductsByDescription(
         @RequestParam description: String
-    ): ResponseEntity<List<Product>> {
+    ): ResponseEntity<out Any>? {
         val result = repository.findByDescriptionContaining(description)
         if(result.isEmpty()) {
-            return ResponseEntity(HttpStatus.NOT_FOUND)
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product with desired description not found")
         }
         return ResponseEntity.ok(result)
     }
