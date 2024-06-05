f2c8341236c42e3c1a074d6f75d2115cbf708079
jandam15 <97833075+jandam15@users.noreply.github.com>
2024-06-05 17:20:22 +0200

    Add method to Client entity interface - Find all clients with id not null and all wishes and products

--- src/main/kotlin/dk/cngroup/wishlist/entity/Client.kt
+++ src/main/kotlin/dk/cngroup/wishlist/entity/Client.kt
@@ -1,12 +1,15 @@
 package dk.cngroup.wishlist.entity
 
 import com.fasterxml.jackson.annotation.JsonManagedReference
+import jakarta.persistence.CascadeType
+import jakarta.persistence.Entity
+import jakarta.persistence.OneToMany
+import jakarta.persistence.OrderColumn
 import org.hibernate.annotations.Formula
 import org.hibernate.annotations.Where
 import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.rest.core.annotation.RestResource
-import jakarta.persistence.*
 
 @Entity
 @Where(clause = "active = true")  // all SELECT statements will be enhanced by where condition; cannot be inherited
@@ -38,4 +41,7 @@ interface ClientRepository : JpaRepository<Client, Long> {
 
     @EntityGraph(attributePaths = ["wishes.products"])
     fun findClientByUserName(userName: String): Client
+
+    @EntityGraph(attributePaths = ["wishes.products"])
+    fun findByIdIsNotNull(): List<Client>
 }
